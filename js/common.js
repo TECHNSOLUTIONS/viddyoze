8122d9edd32ffc28a5e30e57a85bc3612da99344
vladstt <mihaivladro@yahoo.com>
2015-04-26 22:22:04 +0200

    added my videos page

--- js/common.js
+++ js/common.js
@@ -22,4 +22,33 @@ $(document).ready(function() {
         $('.register-holder').fadeOut();
         $('.happy-people').fadeOut();
     });
+
+    $('.custom-label, .label_radio').click(function(){
+        setupLabel();
+
+    });
+    setupLabel();
+
 });
+
+
+function setupLabel() {
+    if ($('.custom-label input').length) {
+        $('.custom-label').each(function(){
+            $(this).removeClass('checked');
+            $('.delete-all').hide();
+        });
+        $('.custom-label input:checked').each(function(){
+            $(this).parent('label').addClass('checked');
+            $('.delete-all').show();
+        });
+    };
+    if ($('.label_radio input').length) {
+        $('.label_radio').each(function(){
+            $(this).removeClass('r_on');
+        });
+        $('.label_radio input:checked').each(function(){
+            $(this).parent('label').addClass('r_on');
+        });
+    };
+};
